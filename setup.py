dbd37487b9343aff4129579245a7ebf8c515e9d4
opanded <emaiofopanded@outlook.com>
2023-09-29 16:37:22 +0000

    modified:   setup.py 	deleted:    setup.py.bak 	new file:   setup.sh

--- setup.py
+++ setup.py
@@ -6,8 +6,5 @@ setup(name='iros22_darl1n',
       author='BaoqianWang',
       packages=find_packages(),
       include_package_data=True,
-      zip_safe=False,
-      license='MIT', # 添加了license参数
-      install_requires=['tensorflow==1.13.1', 'numpy==1.16.4', 'gym==0.13.0', 'mpi4py==3.1.4', 'protobuf==3.19.4', 'imageio==2.21.1', 'matplotlib==3.5.3', 'joblib==1.1.0'], # 添加了install_requires参数
-      python_requires='>=3.7,<3.8' # 添加了python_requires参数
+      zip_safe=False
 )
---
+++ setup.sh
@@ -0,0 +1,3 @@
+apt install -y mpich
+pip install -r requirement.txt
+pip install -e .
