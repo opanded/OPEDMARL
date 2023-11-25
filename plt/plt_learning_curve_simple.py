e821f956885bd515dcf13150c6bc0c164f471ff9
opanded <emaiofopanded@outlook.com>
2023-11-25 15:03:28 +0000

    modified:   maddpg_o/experiments/train_helper/__pycache__/train_helpers.cpython-37.pyc 	new file:   plt/.vscode/settings.json 	new file:   plt/adversarial/12agents.svg 	new file:   plt/adversarial/24agents.svg 	new file:   plt/adversarial/6agents.svg 	new file:   plt/disembark/12agents.png 	new file:   plt/logs/epc_adversarial_seed0-0/evaluator.csv 	new file:   plt/logs/epc_adversarial_seed0-1/evaluator.csv 	new file:   plt/logs/epc_adversarial_seed0-2/evaluator.csv 	new file:   plt/plt_learing_curve_using_rl_plotter.py 	new file:   plt/plt_learning_curve_simple.py 	new file:   plt/print_episode.py 	new file:   plt/print_list.py 	new file:   plt/simple_spread/12agents.svg 	new file:   plt/simple_spread/6agents.svg 	new file:   result/adversarial/epc/16agents/16agents_1/adv_agent.pkl 	modified:   result/adversarial/epc/16agents/16agents_1/agent10.trainable-weights 	modified:   result/adversarial/epc/16agents/16agents_1/agent11.trainable-weights 	modified:   result/adversarial/epc/16agents/16agents_1/agent12.trainable-weights 	modified:   result/adversarial/epc/16agents/16agents_1/agent13.trainable-weights 	modified:   result/adversarial/epc/16agents/16agents_1/agent14.trainable-weights 	modified:   result/adversarial/epc/16agents/16agents_1/agent15.trainable-weights 	modified:   result/adversarial/epc/16agents/16agents_1/agent8.trainable-weights 	modified:   result/adversarial/epc/16agents/16agents_1/agent9.trainable-weights 	new file:   result/adversarial/epc/16agents/16agents_1/global_time.pkl 	new file:   result/adversarial/epc/16agents/16agents_1/good_agent.pkl 	new file:   result/adversarial/epc/16agents/16agents_1/train_index.pkl 	new file:   result/adversarial/epc/16agents/16agents_1/train_time.pkl 	new file:   result/adversarial/epc/32agents/32agents_1/summaries/events.out.tfevents.1697288603.01d4fa30f1c0

---
+++ plt/plt_learning_curve_simple.py
@@ -0,0 +1,158 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pickle
+# todo episode
+# simple_spread
+# 6 agents
+# episode_file = open(r'../result/simple_spread/maddpg/6agents/6agents_1/train_index.pkl','rb')
+# episode = pickle.load(episode_file) # list
+# print(episode)
+
+# 12agents
+# episode_file = open(r'../result/simple_spread/maddpg/12agents/12agents_1/train_index.pkl','rb')
+# episode = pickle.load(episode_file) # list
+# print(episode)
+
+# 24agents
+# episode_file = open(r'../result/simple_spread/maddpg/24agents/24agents_1/train_index.pkl','rb')
+# episode = pickle.load(episode_file) # list
+# print(episode)
+
+# adversarial
+# 6agents
+# episode_file = open(r'../result/adversarial/maddpg/6agents/6agents_1/train_index.pkl','rb')
+# episode = pickle.load(episode_file) # list
+# print(episode)
+
+# 12agents
+# episode_file = open(r'../result/adversarial/maddpg/12agents/12agents_1/train_index.pkl','rb')
+# episode = pickle.load(episode_file) # list
+# print(episode)
+
+# 24agents
+# episode_file = open(r'../result/adversarial/maddpg/24agents/24agents_1/train_index.pkl','rb')
+# episode = pickle.load(episode_file) # list
+# print(episode)
+
+# 12agents disembark
+# episode_file = open(r'../result/disembark/maddpg/12agents/12agents_1/train_index.pkl','rb')
+# episode = pickle.load(episode_file) # list
+# print(episode)
+
+
+# todo reward
+# simple_spread
+# 6agents
+# MADDPG_reward_file = open(r'../result/simple_spread/maddpg/6agents/6agents_1/good_agent.pkl','rb')
+# MADDPG_reward = pickle.load(MADDPG_reward_file) # list
+# print(MADDPG_reward)
+#
+# MFAC_reward_file = open(r'../result/simple_spread/mean_field/6agents/6agents_1/good_agent.pkl','rb')
+# MFAC_reward = pickle.load(MFAC_reward_file) # list
+# print(MFAC_reward)
+#
+# epc_reward_file = open(r'../result/simple_spread/epc/6agents/6agents_1good_agent.pkl','rb')
+# epc_reward = pickle.load(epc_reward_file) # list
+# print(epc_reward)
+
+
+# 12agents
+MADDPG_reward_file = open(r'../result/simple_spread/maddpg/12agents/12agents_1/good_agent.pkl','rb')
+MADDPG_reward = pickle.load(MADDPG_reward_file) # list
+print(MADDPG_reward)
+
+MFAC_reward_file = open(r'../result/simple_spread/mean_field/12agents/12agents_1/good_agent.pkl','rb')
+MFAC_reward = pickle.load(MFAC_reward_file) # list
+print(MFAC_reward)
+
+epc_reward_file = open(r'../result/simple_spread/epc/12agents/12agents_1good_agent.pkl','rb')
+epc_reward = pickle.load(epc_reward_file) # list
+print(epc_reward)
+
+
+# 24agents
+# MADDPG_reward_file = open(r'../result/adversarial/maddpg/24agents/24agents_1/good_agent.pkl','rb')
+# MADDPG_reward = pickle.load(MADDPG_reward_file) # list
+# print(MADDPG_reward)
+#
+# MFAC_reward_file = open(r'../result/adversarial/mean_field/24agents/24agents_1/good_agent.pkl','rb')
+# MFAC_reward = pickle.load(MFAC_reward_file) # list
+# print(MFAC_reward)
+#
+# epc_reward_file = open(r'../result/adversarial/epc/24agents/24agents_1good_agent.pkl','rb')
+# epc_reward = pickle.load(epc_reward_file) # list
+# print(epc_reward)
+
+# adversarial
+# 6agents
+# MADDPG_reward_file = open(r'../result/adversarial/maddpg/6agents/6agents_1/good_agent.pkl','rb')
+# MADDPG_reward = pickle.load(MADDPG_reward_file) # list
+# print(MADDPG_reward)
+#
+# MFAC_reward_file = open(r'../result/adversarial/mean_field/6agents/6agents_1/good_agent.pkl','rb')
+# MFAC_reward = pickle.load(MFAC_reward_file) # list
+# print(MFAC_reward)
+#
+# epc_reward_file = open(r'../result/adversarial/epc/6agents/6agents_1/good_agent.pkl','rb')
+# epc_reward = pickle.load(epc_reward_file) # list
+# print(epc_reward)
+
+
+# 12agents
+# MADDPG_reward_file = open(r'../result/adversarial/maddpg/12agents/12agents_1/good_agent.pkl','rb')
+# MADDPG_reward = pickle.load(MADDPG_reward_file) # list
+# print(MADDPG_reward)
+#
+# MFAC_reward_file = open(r'../result/adversarial/mean_field/12agents/12agents_1/good_agent.pkl','rb')
+# MFAC_reward = pickle.load(MFAC_reward_file) # list
+# print(MFAC_reward)
+#
+# epc_reward_file = open(r'../result/adversarial/epc/12agents/12agents_1good_agent.pkl','rb')
+# epc_reward = pickle.load(epc_reward_file) # list
+# print(epc_reward)
+
+
+# 24agents
+# MADDPG_reward_file = open(r'../result/adversarial/maddpg/24agents/24agents_1/good_agent.pkl','rb')
+# MADDPG_reward = pickle.load(MADDPG_reward_file) # list
+# print(MADDPG_reward)
+#
+# MFAC_reward_file = open(r'../result/adversarial/mean_field/24agents/24agents_1/good_agent.pkl','rb')
+# MFAC_reward = pickle.load(MFAC_reward_file) # list
+# print(MFAC_reward)
+#
+# epc_reward_file = open(r'../result/adversarial/epc/24agents/24agents_1good_agent.pkl','rb')
+# epc_reward = pickle.load(epc_reward_file) # list
+# print(epc_reward)
+
+# disembark
+# 12agents
+# MADDPG_reward_file = open(r'../result/disembark/maddpg/12agents/12agents_1/good_agent.pkl','rb')
+# MADDPG_reward = pickle.load(MADDPG_reward_file) # list
+# print(MADDPG_reward)
+#
+# MFAC_reward_file = open(r'../result/disembark/mean_field/12agents/12agents_1/good_agent.pkl','rb')
+# MFAC_reward = pickle.load(MFAC_reward_file) # list
+# print(MFAC_reward)
+
+plt.plot(episode, MADDPG_reward, color='blue', label='MADDPG')
+plt.plot(episode, MFAC_reward, color='green', label='MFAC')
+plt.plot(episode, epc_reward, color='red', label='MA-ESRL')
+plt.legend()  # 显示图例
+plt.title('learning curve')
+plt.xlabel('episode')
+plt.ylabel('reward')
+
+# simple spread
+plt.savefig('./simple_spread/12agents.svg',format="svg")
+
+# adversarial
+# plt.savefig('./adversarial/24agents.png')
+# plt.savefig('./adversarial/24agents.svg',format="svg")
+
+# disembark
+# plt.savefig('./disembark/12agents.png')
+
+
+plt.show()
---
+++ plt/print_episode.py
@@ -0,0 +1,201 @@
+import pickle
+# rb是2进制编码文件，文本文件用r
+# 每隔30步输出奖励值
+# todo 输出good奖励 (收敛奖励)
+# simple_spread
+# 6agents
+# f = open(r'../result/simple_spread/maddpg/6agents/6agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+#
+# f = open(r'../result/simple_spread/mean_field/6agents/6agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+#
+# f = open(r'../result/simple_spread/epc/6agents/6agents_1good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# 12agents
+# f = open(r'../result/adversarial/maddpg/12agents/12agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/mean_field/12agents/12agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/epc/12agents/12agents_1good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# 24agents
+# f = open(r'../result/simple_spread/maddpg/24agents/24agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/simple_spread/mean_field/24agents/24agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/simple_spread/epc/24agents/24agents_1good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# adversarial
+# 6agents
+# f = open(r'../result/adversarial/maddpg/6agents/6agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/mean_field/6agents/6agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/epc/6agents/6agents_3/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# 12agents
+# f = open(r'../result/adversarial/maddpg/12agents/12agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/mean_field/12agents/12agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/epc/12agents/12agents_1good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# 24agents
+# f = open(r'../result/adversarial/maddpg/24agents/24agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/mean_field/24agents/24agents_1/good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/epc/24agents/24agents_1good_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# todo 输出global_time (训练时间)
+# simple_spread
+# 6agents
+# f = open(r'../result/simple_spread/maddpg/6agents/6agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+#
+# f = open(r'../result/simple_spread/mean_field/6agents/6agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+#
+# f = open(r'../result/simple_spread/epc/6agents/6agents_1global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# 12agents
+# f = open(r'../result/simple_spread/maddpg/12agents/12agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+#
+# f = open(r'../result/simple_spread/mean_field/12agents/12agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+#
+# f = open(r'../result/simple_spread/epc/12agents/12agents_1global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# 24agents
+# f = open(r'../result/simple_spread/maddpg/24agents/24agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/simple_spread/mean_field/24agents/24agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/epc/24agents/24agents_1global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# adversarial
+# 6agents
+# f = open(r'../result/adversarial/maddpg/6agents/6agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/mean_field/6agents/6agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/epc/6agents/6agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# 12agents
+# f = open(r'../result/adversarial/maddpg/12agents/12agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/mean_field/12agents/12agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/epc/12agents/12agents_1global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# 24agents
+# f = open(r'../result/adversarial/maddpg/24agents/24agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/mean_field/24agents/24agents_1/global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# f = open(r'../result/adversarial/epc/24agents/24agents_1global_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+
+# todo 输出adv奖励
+# f = open(r'../result/adversarial/baseline_maddpg/6agents/6agents_1/adv_agent.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+# todo 输出train_time
+# f = open(r'../result/adversarial/maddpg/6agents/6agents_1/train_time.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# todo 输出train_index (回合数)
+# 6agents
+# MADDPG
+# f = open(r'../result/adversarial/maddpg/6agents/6agents_1/train_index.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# MFAC
+# f = open(r'../result/adversarial/epc/6agents/6agents_3/train_index.pkl','rb')
+# data = pickle.load(f)
+# print(data)
+
+# EPC
+# f = open(r'../result/adversarial/epc/24agents/24agents_1train_index.pkl', 'rb')
+# data = pickle.load(f)
+# print(data)
+
+
+
+
+
+
+
+
+
+
